1ba46b29ea770a5d26111a873cd9a1a4d3b7ac61
Vhornets <vhornets@gmail.com>
2013-11-26 21:33:33 +0200

    Init commit

---
+++ domain.js
@@ -0,0 +1,41 @@
+/*global require, exports */
+
+(function () {
+    "use strict";
+
+    var child_process = require("child_process"),
+        domainName = "builder.execute";
+
+    function exec(directory, command, callback) {
+        child_process.exec(command, { cwd: directory}, function (err, stdout, stderr) {
+            callback(err ? stderr : undefined, err ? undefined : stdout);
+        });
+    }
+	
+    exports.init = function (DomainManager) {
+        if (!DomainManager.hasDomain(domainName)) {
+            DomainManager.registerDomain(domainName, {
+                major: 0,
+                minor: 1
+            });
+        }
+        
+        DomainManager.registerCommand(domainName, "exec", exec, true, "Exec cmd",
+            [
+                {
+                    name: "directory",
+                    type: "string"
+                },
+                {
+                    name: "command",
+                    type: "string"
+                }
+            ],
+            [{
+                name: "stdout",
+                type: "string"
+            }]
+        );
+    };
+    	
+}());
